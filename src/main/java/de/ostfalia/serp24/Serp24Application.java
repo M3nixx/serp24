66792c4073b9e3dd56c1cdd299e927824317a522
Nils Winkler <ni.winkler@ostfalia.de>
2025-10-01 12:26:11 +0200

    first

---
+++ src/main/java/de/ostfalia/serp24/Serp24Application.java
@@ -0,0 +1,13 @@
+package de.ostfalia.serp24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serp24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Serp24Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfalia/serp24/Serp24ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfalia.serp24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serp24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
